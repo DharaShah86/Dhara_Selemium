04b9fb5330bf431e40e90daa5ce2cbb42f5c6ab0
DharaShah86 <shahdhara86@gmail.com>
2022-03-22 00:28:09 -0500

    testing by dhara

--- src/test/java/com/sel/test/CreateUserAccount.java
+++ src/test/java/com/sel/test/CreateUserAccount.java
@@ -2,19 +2,73 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
+
 package com.sel.test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author itexps
+ */
+public class GoogleSearchTest {
+    
+    public GoogleSearchTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void testSearch(){
+        // Sutomation code
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://www.google.com");
+        driver.findElement(By.name("q"));
+        
+        
+    }
+}
+
+/*package com.sel.test;
 
 import static com.github.dockerjava.transport.DomainSocket.get;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.time.Duration;
-/*import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-*/
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -29,19 +83,19 @@ public class CreateUserAccount {
     public CreateUserAccount() {
     }
     
-    //@BeforeAll
+    @BeforeAll
     public static void setUpClass() {
     }
     
-    //@AfterAll
+    @AfterAll
     public static void tearDownClass() {
     }
     
-    //@BeforeEach
+    @BeforeEach
     public void setUp() {
     }
     
-    //@AfterEach
+    @AfterEach
     public void tearDown() {
     }
 
@@ -51,7 +105,7 @@ public class CreateUserAccount {
     // @Test
     // public void hello() {}
     
-    //@Test
+   @Test
    public void testCreateUser() throws Exception {
     WebDriverManager.chromedriver().setup();
     WebDriver driver =new ChromeDriver();
@@ -97,4 +151,4 @@ public class CreateUserAccount {
     
     
   }
-}
+}*/
